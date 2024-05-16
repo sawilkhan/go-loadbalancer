2a3e8e9888c0a3859ef41d51480162e96a36f7a7
sawilkhan <68275664+sawilkhan@users.noreply.github.com>
2024-05-16 18:05:03 +0530

    created func main and Server methods

--- src/main.go
+++ src/main.go
@@ -13,6 +13,16 @@ type simpleServer struct {
 	proxy *httputil.ReverseProxy
 }
 
+func (s *simpleServer) Address() string{
+	return s.addr
+}
+
+func (s *simpleServer) IsAlive() bool{
+	return true
+}
+
+func (s *simpleServer) Serve(w http.ResponseWriter, r *http.Request){}
+
 type Server interface{
 	Address() string
 	IsAlive() bool
@@ -36,9 +46,40 @@ type LoadBalancer struct{
 }
 
 
+func NewLoadBalancer(port string, servers []Server) *LoadBalancer{
+	return &LoadBalancer{
+		port: port,
+		servers: servers,
+		roundRobinCount: 0,
+	}
+}
+
 func handleErr(err error){
 	if err != nil{
 		fmt.Printf("error: %v\n", err)
 		os.Exit(1)
 	}
+}
+
+func (lb *LoadBalancer) getNextAvailableServer() Server{}
+
+func (lb *LoadBalancer) serveProxy(w http.ResponseWriter, r *http.Request){}
+
+func main(){
+	servers:= []Server{
+		newSimpleServer("https://www.facebook.com"),
+		newSimpleServer("http://www.bing.com"),
+		newSimpleServer("http://www.duckduckgo.com"),
+	}
+
+	lb := NewLoadBalancer("8000", servers)
+
+	handleRedirect := func(w http.ResponseWriter, r *http.Request){
+		lb.serveProxy(w, r)
+	}
+
+	http.HandleFunc("/", handleRedirect)
+	
+	fmt.Printf("serving requests at 'localhost:%s'\n", lb.port)
+	http.ListenAndServe(":"+lb.port, nil)
 }
